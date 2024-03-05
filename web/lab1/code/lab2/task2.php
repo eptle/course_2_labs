09d424a8f887865b19508b6df45fbc03484f57bf
eptle <btlk04@gmail.com>
2024-03-05 21:44:10 +0200

    add task2.php

---
+++ web/lab1/code/lab2/task2.php
@@ -0,0 +1,16 @@
+<?php
+$integer = 100;
+
+echo $integer;
+echo "\n";
+#-----
+$float = 1.234;
+
+echo "$float\n";
+#-----
+echo 1 + 11 . "\n";
+#-----
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+
+echo abs($lastMonth - $thisMonth) . "\n";
